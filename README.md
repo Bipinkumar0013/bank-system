2a2fef071a3452e927a2c32020fad9e8ab60e647
Bipin Kumar <bkbipin231006@gmail.com>
2026-01-18 15:16:56 +0530

    new code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # bank-system
 This is my first git repository
 <br>
-Author Bipin kumar prajapati
+Author - Bipin kumar prajapati
---
+++ hello.java
@@ -0,0 +1,8 @@
+// package bank-system;
+
+public class hello {
+  public static void main(String[] args) {
+    System.out.print("hello");
+  }
+  
+}
